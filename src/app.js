916ac5ab92fa065b1c0955529ef5cf34b30b2be8
Ahmad-078 <ahmadsufiyan078@gmail.com>
2026-02-23 14:37:27 +0530

    Exploring with different http methods

--- src/app.js
+++ src/app.js
@@ -1,5 +1,13 @@
 const express = require('express');
 const app = express();
+ app.get("/user", (req,res) => {
+    res.send({firstName: "John", lastName: "Doe"});
+ });
+
+ app.post("/user",(req,res) =>{
+    res.send("User created successfully");
+ })
+
  app.use("/test", (req,res) => {
     res.send("I am testing my server")
  });
