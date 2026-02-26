129428432648d7c0691ea683cd3e4a7d68755706
Ahmad-078 <ahmadsufiyan078@gmail.com>
2026-02-26 14:28:47 +0530

    created routes for updating and deleting users

--- src/app.js
+++ src/app.js
@@ -5,7 +5,37 @@ const User = require ('./models/user');
 
 app.use(express.json());
 
+app.get('/feed',async (req,res) => {
+    try{
+        const users = await User.find({});
+        if(users.length === 0){
+            res.status(404).send('No users found!!');
+        }
+        else{
+            res.send(users);
+        }
+    }
+    catch(error){
+        res.status(500).send('Error fetching users data!!'+ error.message); 
+    }
+})
+ 
+app.get('/user', async (req,res)=>{
+     const userEmail = req.body.emailId;
+     try{
+        const user = await User.findOne({emailId:userEmail});
+        if(!user){
+            res.status(404).send('User not found!!');
 
+        }
+        else {
+            res.send(user);
+        }
+     }
+     catch(error){
+        res.status(500).send('Error fetching user data!!'+ error.message);
+     }
+})
 app.post('/signup',async (req , res)=> {
     const user = new User(req.body);
        
@@ -17,6 +47,29 @@ app.post('/signup',async (req , res)=> {
         res.status(500).send('user creation failed!!'+ error.message);
     }
 });
+app.patch('/signup', async (req,res) =>{
+    const userId = req.body._id;
+    try {
+        await User.findByIdAndUpdate(userId,{firstName: "Raiyaan"});
+        res.send ('User updated successfully!!');
+    }
+    catch(error){
+        res.status(500).send('Error updating user data!!'+ error.message);
+    }
+
+});
+
+ app.delete('/signup', async(req,res) => {
+    const userId = req.body._id;
+
+    try{
+         await  User.findByIdAndDelete(userId);
+         res.send('user deleted successfully!!');
+    }
+    catch (error){
+        res.status(500).send('Could not delete user' + error.message);
+    }
+ })
 
 connectionDB().then(()=> {
     console.log('Database connected successfully');
