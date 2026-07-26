6a564cf35d2d69fbcafa74d3816142cf2012247c
Ahmad-078 <ahmadsufiyan078@gmail.com>
2026-07-26 16:11:15 +0530

    Added validator package

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 const userSchema = new mongoose.Schema({
     firstName:{
         type:String,
@@ -15,10 +16,20 @@ const userSchema = new mongoose.Schema({
         tolowercase:true,
         unique:true,
         trim:true,
+         validate: function(value){
+            if(!validator.isEmail(value)){
+                 throw new Error('Please enter a valid email address');
+            }
+         }
     },
     password:{
         type:String,
         required:true,
+        validate(value){
+            if(!validator.isStrongPassword(value)){
+                 throw new Error ('Please enter a strong password');
+            }
+        }
     },
     age:{
         type:Number,
