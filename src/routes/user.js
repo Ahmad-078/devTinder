5275c1707fa718fc0914d3750a678d34d2c76563
Ahmad-078 <ahmadsufiyan078@gmail.com>
2026-08-02 14:41:38 +0530

    api for request received + connection

---
+++ src/routes/user.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const userRouter = express.Router();
+const {userAuth} = require('../middlewares/auth');
+const ConnectionRequest = require("../models/connectionRequest")
+const User_Safe_Data = "firstName lastName age skills gender"
+
+userRouter.get('/user/request/received',userAuth,async (req,res)=>{
+    try{
+       const loggedInUser = req.user;
+       const connectionRequest =  await ConnectionRequest.find({
+        toUserId : loggedInUser._id,
+        status : "interested"
+       }).populate("fromUserId",User_Safe_Data)
+      
+       res.json({
+        message: "Connection request received",
+        data: connectionRequest
+       })
+    
+    
+    }
+    catch(error){
+        res.status(400).send("Error :"+ error.message);
+    }
+
+})
+userRouter.get('/user/connections',userAuth, async(req,res)=>{
+    try{
+        const loggedInUser = req.user;
+        const  connections = await ConnectionRequest.find({
+            $or:[
+                {fromUserId: loggedInUser._id, status:"accepted"},
+                {toUserId: loggedInUser._id, status:"accepted"}
+            ]
+        }).populate("fromUserId",User_Safe_Data).populate("toUserId",User_Safe_Data);
+
+        const data = connections.map(connection =>{
+            if(connection.fromUserId._id.equals(loggedInUser._id)){
+                return connection.toUserId;
+            }
+            return connection.fromUserId;
+        })
+        res.json({
+            message: "Connections fetched successfully",
+            data: data
+        })
+    }
+    catch(error){
+        res.status(400).send("Error :"+ error.message);
+    }
+})
+
+module.exports = userRouter;
