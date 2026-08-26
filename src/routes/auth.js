998db89a9a68f191ecab7f9107fee31fba629ad3
Ahmad-078 <ahmadsufiyan078@gmail.com>
2026-08-26 17:57:09 +0530

    made change for frontend

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -17,8 +17,14 @@ authRouter.post('/signup', async (req,res)=>{
             email,
             password:hashedPassword 
         })
-         await user.save();
-         res.send("User added successfully");
+         const savedUser = await user.save();
+    const token = await savedUser.getJWT();
+
+    res.cookie("token", token, {
+      expires: new Date(Date.now() + 8 * 3600000),
+    });
+
+    res.json({ message: "User Added successfully!", data: savedUser });
     } catch (error) {
         res.status(400).send("Error :"+ error.message);
     }
